17adabf4e4ef39cb4138b1683e59c4fbb47c9f2a
Alex <o.pylkevych@gmail.com>
2016-04-20 20:46:17 +0300

    Create youtrack2redmineMap.py

---
+++ youtrack2redmineMap.py
@@ -0,0 +1,33 @@
+type2tracker = {
+    'Bug': 'Bug',
+    'Feature': 'Feature',
+    'Task': 'Task',
+    'Defect': 'Defect',
+}
+
+state2status = {
+    'Open': 'New',
+    'In Progress': 'In Progress',
+    'Code Review': 'Code Review',
+    'Fixed': 'Resolved',
+    'Verified': 'Verified',
+    'Closed': 'Closed',
+}
+
+priority = {
+    'Minor': 'Low',
+    'Normal': 'Normal',
+    'Major': 'High',
+    'Critical': 'Urgent',
+    'Show-stopper': 'Immediate',
+}
+
+affected_version = {
+    '20-04-2016': '04'
+}
+
+other_fields = {
+    'Estimation': 'Story points',
+    'PlanFix date': 'Planned Date',
+    'Affected version': 'Affected Versions'
+}
